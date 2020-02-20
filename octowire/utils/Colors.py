5db82e9355d3a0353e91c4600d736595b1d89681
ghecko <ghecko78@gmail.com>
2020-02-20 16:37:56 +0100

    change Colors class

--- octowire/utils/Colors.py
+++ octowire/utils/Colors.py
@@ -1,10 +1,10 @@
 # Common Colors
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = '\x1b[95m'
+    OKBLUE = '\x1b[94m'
+    OKGREEN = '\x1b[92m'
+    WARNING = '\x1b[93m'
+    FAIL = '\x1b[91m'
+    ENDC = '\x1b[0m'
+    BOLD = '\x1b[1m'
+    UNDERLINE = '\x1b[4m'
